0a9b78f1ed83c1717b625bc13ff856bec2368cfb
Aditya Upadhyay <adiupd123@gmail.com>
2022-08-23 22:25:30 +0530

    Added 2 features
    1. Taking Customer's Name as Input using EditText\n2. Adding the price of each of the toppings selected to the base-price of coffee per cup\n3. Limited the no. of coffees that can be ordered from 1 - 100

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.justjava;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,7 +15,7 @@ import androidx.appcompat.app.AppCompatActivity;
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 0;
+    int quantity = 90;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
      * A method that increments the current value of quantityTextView by 1
      */
     public void increment(View view){
+        if(quantity>=100){
+            Toast toast = Toast.makeText(this, "You cannot order more than 100 coffees", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
         quantity = quantity + 1;
         displayQuantity(quantity);
     }
@@ -32,9 +39,13 @@ public class MainActivity extends AppCompatActivity {
     /**
      * A method that decrements the current value of quantityTextView by 1
      */
-    public void decrement(View view){
-        if(quantity>0)
-            quantity = quantity - 1;
+    public void decrement(View view) {
+        if (quantity <=1){
+            Toast toast = Toast.makeText(this, "You cannot order less than 1 coffee", Toast.LENGTH_SHORT);
+            toast.show();
+            return;
+        }
+        quantity = quantity - 1;
         displayQuantity(quantity);
     }
 
@@ -42,14 +53,19 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = calculatePrice();
-
         CheckBox whippedCreamCheckBox = (CheckBox) findViewById(R.id.whippedCream_checkBox);
         boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
 
         CheckBox chocolateCheckBox = (CheckBox) findViewById(R.id.chocolate_checkBox);
         boolean hasChocolate = chocolateCheckBox.isChecked();
-        String message = createOrderSummary(price, hasWhippedCream, hasChocolate);
+
+        int totalPrice = calculatePrice(hasWhippedCream, hasChocolate);
+
+        EditText editText = findViewById(R.id.name_editText);
+        String name = editText.getText().toString();
+        // Editable object is returned by getTex() method
+
+        String message = createOrderSummary(name, totalPrice, hasWhippedCream, hasChocolate);
         displayMessage(message);
     }
 
@@ -63,24 +79,32 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * This method calculates price of coffees ordered
+     * @param addedWhippedCream
+     * @param addedChocolate
      * @return price of specified no. of coffees ordered
      */
-    private int calculatePrice() {
-        int rate = 5;
-        return quantity * rate;
+    private int calculatePrice(boolean addedWhippedCream, boolean addedChocolate) {
+        int rateOfCoffee = 5, rateOfWhippedCream = 1, rateOfChocolate = 2;
+        int basePrice = rateOfCoffee;
+        if(addedWhippedCream)
+            basePrice += rateOfWhippedCream;
+        if(addedChocolate)
+            basePrice += rateOfChocolate;
+        return basePrice * quantity;
     }
 
     /**
      * This method creates the message containing entire order summary and returns it
+     * @param name
      * @param totalPrice
-     * @param addWhippedCream
-     * @param addChocolate
-     * @return
+     * @param addedChocolate
+     * @param addedWhippedCream
+     * @return String containing Order Details
      */
-    private String createOrderSummary(int totalPrice, boolean addWhippedCream, boolean addChocolate){
-        String message = "Name: Kaptan Kunal" +
-                "\nAdd Whipped Cream? " + addWhippedCream +
-                "\nAdd Chocolate? " + addChocolate +
+    private String createOrderSummary(String name, int totalPrice, boolean addedWhippedCream, boolean addedChocolate){
+        String message = "Name: " + name +
+                "\nAdd Whipped Cream? " + addedWhippedCream +
+                "\nAdd Chocolate? " + addedChocolate +
                 "\nQuantity: " + quantity +
                 "\nTotal: $" + totalPrice +
                 "\nThank You!";
